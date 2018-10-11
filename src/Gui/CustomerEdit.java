6139dc8a441ce4786b94615a76d943759df66478
AviHalif <44060035+AviHalif@users.noreply.github.com>
2018-10-11 16:27:55 +0300

    ffgd

--- src/Gui/CustomerEdit.java
+++ src/Gui/CustomerEdit.java
@@ -16,7 +16,6 @@ public class CustomerEdit extends JFrame {
     private Client client;
     private JSONObject jsonObject;
 
-    // fgdfgdfgdfgdfgdfgdfgfdffgf
     private JFrame jFramePrev, twoBackFrame;
     private JPanel jPanelMain;
     private JLabel label_message;
